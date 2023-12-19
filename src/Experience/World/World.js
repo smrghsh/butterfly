ffc2ecb4d0f2b37bde1b667810d5b7362a777a14
Samir Ghosh <hello@samir.tech>
2023-12-18 22:34:13 -0800

    clean

--- src/Experience/World/World.js
+++ src/Experience/World/World.js
@@ -8,8 +8,8 @@ import Floor from "./Floor.js";
 import Stars from "./Stars.js";
 // import DNAbackboneVertexShader from "../../shaders/DNAbackbone/vertex.glsl";
 // import DNAbackboneFragmentShader from "../../shaders/DNAbackbone/fragment.glsl";
-
 import Butterfly from "./Butterfly.js";
+import Butterflies from "./Butterflies.js";
 // import Sushi from './Sushi.js'
 // import Test from './Test.js'
 
@@ -29,72 +29,7 @@ export default class World {
       console.log("resources ready");
       // this.test = new Test()
       this.stars = new Stars();
-      // const lineLength = 5;
-      // const points = [];
-      // const size = [];
-      // const thickness = 0.2;
-      // const speed = 1.0;
-      // const amplitude = 1.0;
-      // const DNA = new THREE.Group();
-      // for (let i = -lineLength; i < lineLength; i += 0.05 * Math.random()) {
-      //   points.push(
-      //     new THREE.Vector3(
-      //       Math.random() * thickness,
-      //       Math.random() * thickness,
-      //       i
-      //     )
-      //   );
-      // }
-      // this.material = new THREE.ShaderMaterial({
-      //   vertexShader: DNAbackboneVertexShader,
-      //   fragmentShader: DNAbackboneFragmentShader,
-      //   uniforms: {
-      //     uTime: { value: 0.0 },
-      //     offset: { value: 0.0 },
-      //     speed: { value: speed },
-      //     amplitude: { value: amplitude },
-      //   },
-      //   side: THREE.DoubleSide,
-      // });
-      // this.material2 = new THREE.ShaderMaterial({
-      //   vertexShader: DNAbackboneVertexShader,
-      //   fragmentShader: DNAbackboneFragmentShader,
-      //   uniforms: {
-      //     uTime: { value: 0.0 },
-      //     offset: { value: 1.0 },
-      //     speed: { value: speed },
-      //     amplitude: { value: amplitude },
-      //   },
-      //   side: THREE.DoubleSide,
-      // });
-      // const geometry = new THREE.BufferGeometry().setFromPoints(points);
-      // // const backbone1 = new THREE.Line( geometry, material );
-      // const backbone1 = new THREE.Mesh(geometry, this.material);
-      // // const backbone2 = new THREE.Line( geometry, material2 );
-      // const backbone2 = new THREE.Points(geometry, this.material2);
-      // // line2.position.z += 8.0;
-      // DNA.add(backbone1);
-      // DNA.add(backbone2);
-      // DNA.rotation.x += 1.111111;
-      // DNA.scale.set(0.5, 0.5, 0.5);
-
-      // for (let i = 0; i < 5; i += 0.12) {
-      //   var DNA2 = DNA.clone();
-      //   DNA2.position.y += i / 10;
-      //   DNA2.position.y += 1;
-      //   DNA2.position.z -= 1;
-      //   DNA2.rotation.y += Math.random();
-      //   DNA2.rotation.x += 1.0 * (Math.random() - 0.0) + i / 4;
-      //   // DNA2.rotation.z +=  Math.PI
-      //   this.scene.add(DNA2);
-      // }
-      // // DNA.position.y += 7;
-      // // DNA2.position.y += 7;
-      // // DNA.position.z -= 8;
-      // // DNA2.position.z -= 8;
-      // this.scene.add(new THREE.AxesHelper());
-      // this.spectra = new Spectra()
-      // this.sushi = new Sushi()
+      this.butterflies = new Butterflies();
       this.butterfly = new Butterfly();
 
       this.environment = new Environment();
@@ -103,13 +38,8 @@ export default class World {
   }
   update() {
     if (this.ready) {
+      this.butterflies.update();
       this.butterfly.update();
-      //   let t = Date.now() - this.experience.time.start;
-      //   this.material.uniforms.uTime.value = t / 1000;
-      //   this.material2.uniforms.uTime.value = t / 1000;
-      //   this.material.uniforms.amplitude.value = 1.0 + 0.2 * Math.sin(t / 4000);
-      //   this.material2.uniforms.amplitude.value = 1.0 + 0.2 * Math.sin(t / 4000);
     }
-    // this.circles.update()
   }
 }
